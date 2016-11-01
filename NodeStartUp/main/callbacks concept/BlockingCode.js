639c6e8d2713f44c10111c365985faf6ee04bdf7
ArronDon <alextuancc@gmail.com>
2016-11-01 18:26:33 +0800

    add event loop

--- NodeStartUp/main/callbacks concept/BlockingCode.js
+++ NodeStartUp/main/callbacks concept/BlockingCode.js
@@ -1,7 +1,7 @@
 /**
  * Created by Arron on 2016/11/1.
  */
-//the program blocks until it reads the file 
+//the program blocks until it reads the file
 var fs = require("fs");
 var data = fs.readFileSync('input.txt');
 console.log(data.toString());
